3f06b1c8748974dd90ccbea1a18e009d6ed298fe
dimside <dimside29@gmail.com>
2023-10-07 16:03:48 +0300

    create-rental-cars-web-app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import AdvertCard from 'components/AdvertCard';
+
+function App() {
+  return (
+    <div>
+      <h1>Rental cars</h1>
+      <AdvertCard />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/AdvertCard/AdvertCard.styled.js
@@ -0,0 +1,27 @@
+import styled from '@emotion/styled';
+
+export const AdvertItem = styled.li`
+  width: 274px;
+  height: 426px;
+`;
+
+export const ImageContainer = styled.div`
+  width: 100%;
+  height: 268px;
+  border-radius: 14px;
+  overflow: hidden;
+`;
+
+export const BrandName = styled.p`
+  color: #121417;
+  font-size: 16px;
+  font-style: normal;
+  line-height: 24px; /* 150% */
+`;
+
+export const InfoContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: 274px;
+  height: 40px;
+`;
